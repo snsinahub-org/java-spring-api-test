4ba76c685e7b0df5b428f07f7f7714df04b69665
snsinahub <snsinahub@github.com>
2023-10-09 14:49:53 -0400

    add

--- src/main/java/com/snsina/mvc/MvcApplication.java
+++ src/main/java/com/snsina/mvc/MvcApplication.java
@@ -12,15 +12,6 @@ public class MvcApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(MvcApplication.class, args);
 	}
 
-	// write a java method named holidays_excluded to calculate the number of days between two dates excluding holidays
-	// @param: start_date
-	// @param: end_date
-	// @return: number of days between two dates excluding holidays
-	public static int holidays_excluded(String start_date, String end_date) {
-		// calculate days between end_date and start_date
-		// return the number of days
-
-
-	}
+	
 
 }
